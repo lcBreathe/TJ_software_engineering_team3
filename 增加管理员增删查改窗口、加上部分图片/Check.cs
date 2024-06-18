29b6662a6983cd618039aebee8a3733690d51168
NieYiran <165036993+NieYiran@users.noreply.github.com>
2024-06-19 01:33:11 +0800

    Add files via upload

---
+++ 增加管理员增删查改窗口、加上部分图片/Check.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace DatabaseProject
+{
+    public partial class Check : Form
+    {
+        public Check()
+        {
+            InitializeComponent();
+        }
+        SqlConnection myconn = new SqlConnection("Data source=ZHANGX;Initial catalog=Seismic_information_platform;Integrated Security=True");
+        private void button3_Click(object sender, EventArgs e)
+        {
+            control cl = new control();
+            cl.Show();
+            this.Hide();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string s = "update 反馈信息 set pass = 1 where info = '" + richTextBox1.Text.Trim() + "'";
+            SqlCommand mycmd = new SqlCommand(s, myconn);
+            if (MessageBox.Show("Confirm upload?", "Confirm Message", MessageBoxButtons.OKCancel) == DialogResult.OK)
+            {
+                try
+                {
+                    MessageBox.Show(s);
+                    myconn.Open();
+                    mycmd.ExecuteNonQuery();
+                    myconn.Close();
+                    richTextBox1.Text = "";
+                    MessageBox.Show("更新成功");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex);
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string s = "delete 反馈信息 where info = '" + richTextBox1.Text.Trim() + "'";                                                     // 删除语句
+            SqlCommand mycmd = new SqlCommand(s, myconn);
+            if (MessageBox.Show("Confirm upload?", "Confirm Message", MessageBoxButtons.OKCancel) == DialogResult.OK)
+            {
+                try
+                {
+                    MessageBox.Show(s);
+                    myconn.Open();
+                    mycmd.ExecuteNonQuery();
+                    myconn.Close();
+                    richTextBox1.Text = "";
+                    MessageBox.Show("更新成功");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex);
+                }
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            SqlDataAdapter myadapter = new SqlDataAdapter("select info from 反馈信息 where pass ='0'", myconn);
+            DataSet myset = new DataSet();
+            myadapter.Fill(myset, "查找");
+            string a = myset.Tables["查找"].Rows[0][0].ToString();
+            richTextBox1.Text = a;
+        }
+    }
+}
